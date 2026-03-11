2a3b5f4abe45ec45ca43060daefc54c7f9a4bd04
simrankasaudhan8-cmd <simrankasaudhan8@gmail.com>
2026-03-11 22:09:09 +0530

    Add files via upload

---
+++ exp-6/exp6.py
@@ -0,0 +1,13 @@
+student = {
+    "name": "Simran",
+    "age": 20,
+    "course": "BCA"
+}
+
+print("Dictionary:", student)
+
+student["age"] = 21
+print("Updated age:", student)
+
+print("Keys:", student.keys())
+print("Values:", student.values())
