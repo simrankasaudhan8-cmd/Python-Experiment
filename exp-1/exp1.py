89f25b3b7fc945b3d566c0f128ac0e93732f6518
simrankasaudhan8-cmd <simrankasaudhan8@gmail.com>
2026-03-11 21:31:18 +0530

    Add files via upload

---
+++ exp-1/exp1.py
@@ -0,0 +1,21 @@
+# Data Types
+a = 10
+b = 5.5
+c = "Python"
+d = True
+
+print("Integer:", a)
+print("Float:", b)
+print("String:", c)
+print("Boolean:", d)
+
+# Operators
+x = 10
+y = 3
+
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
+print("Modulus:", x % y)
+print("Exponent:", x ** y)
